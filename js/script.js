2dbf9cb9d3bfd4d7030e3821ff61b7ce9e59204d
neelop1204 <200556051@student.georgianc.on.ca>
2024-04-19 14:10:00 -0400

    sad

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 function submitForm() {
-    var formData = new FormData(document.querySelector('form'));
+    // Get form data
+    const form = document.querySelector('.questions-section');
+    const formData = new FormData(form);
 
-    fetch('https://neelop1204.github.io/Priyanshi/submit.php', {
+    // Make fetch request
+    fetch('submit.php', {
         method: 'POST',
         body: formData
     })
     .then(response => response.text())
     .then(data => {
-        console.log(data);
-        // Handle success
+        console.log(data); // Should print "Thank You Priyanshi!!" if successful
     })
-    .catch(error => {
+    .catch((error) => {
         console.error('Error:', error);
-        // Handle error
     });
 }
