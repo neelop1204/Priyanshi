8fe13234e7a999af3008b2d1047cd49b5d739f9d
neelop1204 <200556051@student.georgianc.on.ca>
2024-04-19 14:07:42 -0400

    fghjk

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-fetch('https://neelop1204.github.io/Priyanshi/submit.php', {
-    method: 'POST',
-    body: formData
-})
-.then(response => response.json())
-.then(data => {
-    if (data.success) {
-        alert('Thank You Priyanshi!!');
-        window.location.href = 'thankyou.html';
-    } else {
-        alert('Error: ' + data.message);
-    }
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+function submitForm() {
+    var formData = new FormData(document.querySelector('form'));
+
+    fetch('https://neelop1204.github.io/Priyanshi/submit.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        // Handle success
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle error
+    });
+}
